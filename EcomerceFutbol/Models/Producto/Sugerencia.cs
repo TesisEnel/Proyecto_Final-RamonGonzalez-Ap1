167ea5c011d41a563c11edfe486e8a6d3291b4f9
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-08-07 18:40:24 -0400

    Agregando modelo necesario

---
+++ EcomerceFutbol/Models/Producto/Sugerencia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Proyecto_Final.Models
+{
+    public class Sugerencia
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido.")]
+        [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El correo electrónico es requerido.")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo electrónico válido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El mensaje es requerido.")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "El mensaje debe tener entre 10 y 1000 caracteres.")]
+        public string Mensaje { get; set; } = string.Empty;
+
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+    }
+}
