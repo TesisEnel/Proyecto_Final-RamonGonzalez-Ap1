5d689301af9bb0aa7245be23c36887b91c9a3c79
Ramon Gonzalez <ramon_gonzalez2@ucne.edu.do>
2025-08-07 18:39:04 -0400

    Arreglando modelos

--- EcomerceFutbol/Models/Carrito/CarritoItem.cs
+++ EcomerceFutbol/Models/Carrito/CarritoItem.cs
@@ -1,19 +1,24 @@
 ﻿using Proyecto_Final.Models.Producto;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Proyecto_Final.Models.Carrito
 {
     public class CarritoItem
     {
+        [Key]
         public int Id { get; set; }
+
         public int Cantidad { get; set; }
-        public DateTime FechaAgregado { get; set; } = DateTime.UtcNow;
+        public DateTime FechaAgregado { get; set; }
+
         public int CarritoId { get; set; }
+        [ForeignKey("CarritoId")]
         public virtual Carrito Carrito { get; set; }
+
         public int ProductoVariacionId { get; set; }
+        [ForeignKey("ProductoVariacionId")]
         public virtual ProductoVariacion ProductoVariacion { get; set; }
-        public int ProductoId { get; set; }
-        public virtual Proyecto_Final.Models.Producto.Producto Producto { get; set; }
     }
 }
--- EcomerceFutbol/Models/Producto/Valoracion.cs
+++ EcomerceFutbol/Models/Producto/Valoracion.cs
@@ -3,24 +3,36 @@ using Proyecto_Final.Models.Usuario;
 using System.ComponentModel.DataAnnotations;
 
 namespace Proyecto_Final.Models.Producto
-
 {
     public class Valoracion
-
     {
         public int Id { get; set; }
         public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
+        public string? Apellido { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+        public string? CorreoElectronico { get; set; }
+
         [Required]
-        public string UsuarioId { get; set; } 
-        [Required]
+        public string UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "La puntuación es obligatoria.")]
         [Range(1, 5, ErrorMessage = "La puntuación debe estar entre 1 y 5.")]
         public int Puntuacion { get; set; }
-        [MaxLength(1000)]
+
+        [Required(ErrorMessage = "La reseña es obligatoria.")]
+        [StringLength(500, ErrorMessage = "La reseña no puede exceder los 500 caracteres.")]
         public string Comentario { get; set; } = string.Empty;
+
         public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
-        public Producto? Producto { get; set; }
+
+        public Producto? Producto { get; set; }
         public ApplicationUser? Usuario { get; set; }
-
     }
-
 }
